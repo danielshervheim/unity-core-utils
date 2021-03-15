44a3c6022ad1a1ca7d9e0603f02eb83dba23b991
Daniel Shervheim <danielshervheim@gmail.com>
2021-03-14 20:37:01 -0500

    updated SnapRect to use Layout hooks

--- Packages/com.dss.core-utils/Runtime/LayoutUtilities/SnapRect/SnapRect.cs
+++ Packages/com.dss.core-utils/Runtime/LayoutUtilities/SnapRect/SnapRect.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using static DSS.CoreUtils.Extensions.RectTransformExtensions;
 
 namespace DSS.CoreUtils.LayoutUtilities
 {
@@ -11,7 +12,7 @@ namespace DSS.CoreUtils.LayoutUtilities
     [AddComponentMenu("DSS/Layout Utilities/Snap Rect")]	
     [ExecuteInEditMode]
     [RequireComponent(typeof(RectTransform))]
-    public class SnapRect : MonoBehaviour
+    public class SnapRect : MonoBehaviour, ILayoutSelfController
     {
         public enum Mode { Absolute, Relative };
 
@@ -33,80 +34,141 @@ namespace DSS.CoreUtils.LayoutUtilities
         [SerializeField] private float targetHeightAbsolute = 100f;
         [SerializeField][Range(0f,1f)] private float snapAtHeightRelative = 0.5f;
 
-        private RectTransform rect;
-        private RectTransform parentRect;
+        [System.NonSerialized] private RectTransform m_rect;
+        [System.NonSerialized] private RectTransform m_parentRect;
+        private DrivenRectTransformTracker m_tracker;
 
-        private void Update()
+        // private property to get the RectTransform.
+        private RectTransform rectTransform
         {
-            if (rect == null)
+            get
             {
-                rect = GetComponent<RectTransform>();
-            }
-            if (parentRect == null)
-            {
-                parentRect = transform.parent.GetComponent<RectTransform>();
-            }
-
-            if (snapWidth)
-            {
-                Vector2 sizeDelta = rect.sizeDelta;
-                
-                if (widthMode == Mode.Absolute)
+                if (m_rect == null)
                 {
-                    if (targetWidthAbsolute > snapAtWidthRelative*parentRect.rect.size.x)
-                    {
-                        sizeDelta.x = parentRect.rect.size.x;
-                    }
-                    else
-                    {
-                        sizeDelta.x = targetWidthAbsolute;
-                    }
+                    m_rect = GetComponent<RectTransform>();
                 }
-                else if (widthMode == Mode.Relative)
-                {
-                    if (targetWidthRelative*parentRect.rect.size.x > snapAtWidthAbsolute)
-                    {
-                        sizeDelta.x = parentRect.rect.size.x;
-                    }
-                    else
-                    {
-                        sizeDelta.x = targetWidthRelative*parentRect.rect.size.x;
-                    }
-                }
-
-                rect.sizeDelta = sizeDelta;
-            }
-
-
-            if (snapHeight)
-            {
-                Vector2 sizeDelta = rect.sizeDelta;
-                
-                if (heightMode == Mode.Absolute)
-                {
-                    if (targetHeightAbsolute > snapAtHeightRelative*parentRect.rect.size.y)
-                    {
-                        sizeDelta.y = parentRect.rect.size.y;
-                    }
-                    else
-                    {
-                        sizeDelta.y = targetHeightAbsolute;
-                    }
-                }
-                else if (heightMode == Mode.Relative)
-                {
-                    if (targetHeightRelative*parentRect.rect.size.y > snapAtHeightAbsolute)
-                    {
-                        sizeDelta.y = parentRect.rect.size.y;
-                    }
-                    else
-                    {
-                        sizeDelta.y = targetHeightRelative*parentRect.rect.size.y;
-                    }
-                }
-
-                rect.sizeDelta = sizeDelta;
+                return m_rect;
             }
         }
+        private RectTransform parentRectTransform
+        {
+            get
+            {
+                if (m_parentRect == null)
+                {
+                    m_parentRect = rectTransform.parent.GetComponent<RectTransform>();
+                }
+                return m_parentRect;
+            }
+        }
+
+        private void OnEnable()
+        {
+            SetDirty();
+        }
+
+        private void OnDisable()
+        {
+            m_tracker.Clear();
+        }
+
+        public void SetLayoutHorizontal()
+        {
+            m_tracker.Clear();
+
+            UpdateLayout(0);
+        }
+
+        public void SetLayoutVertical()
+        {
+            UpdateLayout(1);
+        }
+
+        private void UpdateLayout(int axis)
+        {
+            // Verify and prepare horizontal axis.
+            if (axis == 0)
+            {
+                if (!snapWidth)
+                {
+                    return;
+                }
+                m_tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDeltaX |
+                                                   DrivenTransformProperties.AnchorMinX |
+                                                   DrivenTransformProperties.AnchorMaxX |
+                                                   DrivenTransformProperties.AnchoredPositionX);
+                rectTransform.SetAnchorMinX(rectTransform.GetPivotX());
+                rectTransform.SetAnchorMaxX(rectTransform.GetPivotX());
+                rectTransform.SetAnchoredPositionX(0f);
+            }
+
+            // Verify and prepare vertical axis.
+            if (axis == 1)
+            {
+                if (!snapHeight)
+                {
+                    return;
+                }
+                m_tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDeltaY |
+                                                   DrivenTransformProperties.AnchorMinY |
+                                                   DrivenTransformProperties.AnchorMaxY |
+                                                   DrivenTransformProperties.AnchoredPositionY);
+                rectTransform.SetAnchorMinY(rectTransform.GetPivotY());
+                rectTransform.SetAnchorMaxY(rectTransform.GetPivotY());
+                rectTransform.SetAnchoredPositionY(0f);
+            }
+
+            // Compute the sizes to be working with.
+            Vector2 sizeDelta = rectTransform.sizeDelta;
+            Mode mode = (axis == 0) ? widthMode : heightMode;
+
+            float targetAbsolute = (axis == 0) ? targetWidthAbsolute : targetHeightAbsolute;
+            float targetRelative = (axis == 0) ? targetWidthRelative : targetHeightRelative;
+            
+            float snapAtAbsolute = (axis == 0) ? snapAtWidthAbsolute : snapAtHeightAbsolute;
+            float snapAtRelative = (axis == 0) ? snapAtWidthRelative : snapAtHeightRelative;
+
+            if (mode == Mode.Absolute)
+            {
+                if (targetAbsolute > snapAtRelative*parentRectTransform.rect.size[axis])
+                {
+                    sizeDelta[axis] = parentRectTransform.rect.size[axis];
+                }
+                else
+                {
+                    sizeDelta[axis] = targetAbsolute;
+                }
+            }
+            else if (mode == Mode.Relative)
+            {
+                if (targetRelative*parentRectTransform.rect.size[axis] > snapAtAbsolute)
+                {
+                    sizeDelta[axis] = parentRectTransform.rect.size[axis];
+                }
+                else
+                {
+                    sizeDelta[axis] = targetRelative*parentRectTransform.rect.size[axis];
+                }
+            }   
+
+            // Set the size delta.
+            rectTransform.sizeDelta = sizeDelta;
+        }
+
+        private void SetDirty()
+        {
+            if (!gameObject.activeSelf)
+            {
+                return;
+            }
+            LayoutRebuilder.MarkLayoutForRebuild(rectTransform);
+        }
+	
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            SetDirty();
+        }
+        #endif
     }
 }
